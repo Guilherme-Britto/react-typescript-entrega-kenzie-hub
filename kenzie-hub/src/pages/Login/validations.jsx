e8384c1f0c2367e766274a2316b34b665be530aa
Guilherme Britto <guilhermefragab@gmail.com>
2023-02-15 19:46:49 -0300

    done

--- kenzie-hub/src/pages/Login/validations.jsx
+++ kenzie-hub/src/pages/Login/validations.jsx
@@ -2,7 +2,7 @@ import * as yup from "yup";
 
 export const schema = yup
   .object({
-    email: yup.string().email().required("Email é obrigatório"),
+    email: yup.string().email("Deve ser um email válido").required("Email é obrigatório"),
     password: yup.string().required("Senha é obrigatório"),
 
   })
